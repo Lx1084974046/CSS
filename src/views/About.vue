fc9e7b9abd37cc463de9240c647d830211ba9f80
Lx1084974046 <1084974046@qq.com>
2021-11-28 18:04:22 +0800

    发牌效果

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,158 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="content">
+      <i :class="ani?'card card'+item+' ani'+item:'card card'+item" v-for="item in 7" :key="item">{{item}}</i>
+    </div>
+    <i class="btn" @click="goAni">发牌</i>
   </div>
 </template>
+
+<script>
+  export default {
+  data() {
+    return {
+      ani:false
+    };
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+  goAni(){
+      this.ani=!this.ani
+    }
+  },
+}
+</script>
+
+<style lang='less' scoped>
+.about{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .btn{
+    width: 80px;
+    height: 50px;
+    background-color: pink;
+    border-radius: 5px;
+    line-height: 50px;
+    text-align: center;
+    cursor: pointer;
+    color:powderblue;
+    position: absolute;
+    top: 500px;
+    left: 720px;
+  }
+  .content{
+    display: flex;
+    position: absolute;
+    top: 200px;
+    left: 680px;
+    .card{
+      width: 160px;
+      height: 200px;
+      background-color: pink;
+      position: absolute;
+      transform-origin: 50% 1020%;
+    }
+    .card1{
+       transform: rotate(-15deg);
+    }
+    .card2{
+      transform: rotate(-10deg);
+      z-index: 2;
+    }
+    .card3{
+      transform: rotate(-5deg);
+      z-index: 3;
+    }
+    .card4{
+      transform: rotate(0deg);
+      z-index: 4;
+    }
+    .card5{
+      transform: rotate(5deg);
+      z-index: 5;
+    }
+    .card6{
+       transform: rotate(10deg);
+       z-index: 6;
+    }
+    .card7{
+       transform: rotate(15deg);
+       z-index: 7;
+    }
+     .ani2{
+  animation: moveAni2 0.12s linear;
+}
+.ani3{
+  animation: moveAni3 0.24s linear;
+}
+.ani4{
+  animation: moveAni4 0.36s linear;
+}
+.ani5{
+  animation: moveAni5 0.48s linear;
+}
+.ani6{
+  animation: moveAni6 0.6s linear;
+}
+.ani7{
+  animation: moveAni7 0.72s linear;
+}
+     @keyframes moveAni2 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(-10deg);
+   }
+ }
+ @keyframes moveAni3 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(-5deg);
+   }
+ }
+  @keyframes moveAni4 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(0deg);
+   }
+ }
+ @keyframes moveAni5 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(5deg);
+   }
+ }
+  @keyframes moveAni6 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(10deg);
+   }
+ }
+ @keyframes moveAni7 {
+   0%{
+      transform: rotate(-15deg);
+   }
+   100%{
+     transform: rotate(15deg);
+   }
+ }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,162 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="query">
+    <div class="box">
+      <!-- 
+      <i :class="ani?'card card1 ani1':'card card1'"></i>
+      <i :class="ani?'card card2 ani2':'card card2'"></i>
+      <i :class="ani?'card card3 ani3':'card card3'"></i>
+      <i :class="ani?'card card4 ani4':'card card4'"></i>
+      <i :class="ani?'card card5 ani5':'card card5'"></i>
+      <i :class="ani?'card card6 ani6':'card card6'"></i> -->
+       <i :class="ani?'card card'+item+' ani'+item:'card card'+item" v-for="item in 6" :key="item"></i>
+    </div>
+    <i class="btn" @click="goAni">发牌</i>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+  data() {
+    return {
+      ani:false
+    };
+  },
+  created() {
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  },
+  mounted() {
+
+  },
+  methods: {
+    goAni(){
+      this.ani=!this.ani
+    }
+  },
 }
 </script>
+
+<style lang='less' scoped>
+.query{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  justify-content: center;
+  .btn{
+    width: 80px;
+    height: 50px;
+    background-color: pink;
+    border-radius: 5px;
+    line-height: 50px;
+    text-align: center;
+    cursor: pointer;
+    color:powderblue
+  }
+  .box{
+    position: fixed;
+    top: 80px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+  }
+  .card{
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 160px;
+    height: 200px;
+    background-color: blue;
+    transform: rotate(-20deg);
+    transform-origin: 50% 675%;
+  }
+ .card2{
+   background-color: red;
+   transform: rotate(-12deg);
+   z-index: 2;
+ }
+ .card3{
+   background-color: yellow;
+   transform: rotate(-4deg);
+   z-index: 3;
+ }
+  .card4{
+   background-color: red;
+   transform: rotate(4deg);
+   z-index: 4;
+ }
+ .card5{
+   background-color: blue;
+   transform: rotate(12deg);
+   z-index: 5;
+ }
+ .card6{
+   background-color: chartreuse;
+   transform: rotate(20deg);
+   z-index: 6;
+ }
+//   .card2{
+//    background-color: red;
+//    transform: rotate(10deg);
+//  }
+//  .card1{
+//    animation: moveAni 1s infinite;
+//  }
+.ani2{
+  animation: moveAni2 0.12s linear;
+}
+.ani3{
+  animation: moveAni3 0.24s linear;
+}
+.ani4{
+  animation: moveAni4 0.36s linear;
+}
+.ani5{
+  animation: moveAni5 0.48s linear;
+}
+.ani6{
+  animation: moveAni6 0.6s linear;
+}
+ @keyframes moveAni2 {
+   0%{
+      transform: rotate(-20deg);
+   }
+   100%{
+     transform: rotate(-12deg);
+   }
+ }
+  @keyframes moveAni3 {
+   0%{
+      transform: rotate(-20deg);
+   }
+   100%{
+     transform: rotate(-4deg);
+   }
+ }
+ @keyframes moveAni4 {
+   0%{
+      transform: rotate(-20deg);
+   }
+   100%{
+     transform: rotate(4deg);
+   }
+ }
+ @keyframes moveAni5 {
+   0%{
+      transform: rotate(-20deg);
+   }
+   100%{
+     transform: rotate(12deg);
+   }
+ }
+ @keyframes moveAni6 {
+   0%{
+      transform: rotate(-20deg);
+   }
+   100%{
+     transform: rotate(20deg);
+   }
+ }
+}
+</style>
