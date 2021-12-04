1c7a7c56144f99fb765b6894a51e1f81f4e4fd07
liuxin <1084974046@qq.com>
2021-12-05 00:35:12 +0800

    fix

--- src/utils/starry.js
+++ src/utils/starry.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// import Stats from "@/utils/stats.js";
 export const initStar = 
 function () {
   'use strict'
